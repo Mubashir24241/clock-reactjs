90cdc6111193f187df4603763b72b38daabccbc7
MUBASHIR <mubashir.ahmad.cse.2020@miet.ac.in>
2024-08-21 15:16:39 +0530

    first commit

---
+++ src/components/ClockSlogan.jsx
@@ -0,0 +1,9 @@
+let ClockSlogan = () => {
+  return (
+    <p className="lead">
+      This is the simple clock that shows the time in India at all times.
+    </p>
+  );
+};
+
+export default ClockSlogan;
